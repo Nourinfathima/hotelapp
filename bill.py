d128025e65a8c78d274d7c380e2f1676ace4694b
nourinfathima <nourinnourin36@gmail.com>
2022-11-03 13:32:18 +0530

    first commit

---
+++ bill.py
@@ -0,0 +1,24 @@
+while(True):
+    print("\n please select an option")
+    print("1 tea")
+    print("2 coffee")
+    print("3 buger")
+    print("4 sandwich")
+    print("5 alpham")
+    print("6 generate bill")
+    print("7 exit")
+    ch=int(input("enter the choice"))
+    if(ch==1):
+        print("added tea")
+    elif(ch==2):
+        print("added coffee")
+    elif(ch==3):
+        print("added buger")
+    elif(ch==4):
+        print("added sandwich")
+    elif(ch==5):
+        print("added alpham")
+    elif(ch==6):
+        print("generating bill")
+    elif(ch==7):
+        exit
